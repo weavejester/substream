96850a20ddfb6fbed4ae73a7b2556e0d12f36942
James Reeves <jreeves@weavejester.com>
2014-01-24 16:17:27 +0000

    Added function to create InputStream from IFn

--- src/clojure/substream/core.clj
+++ src/clojure/substream/core.clj
@@ -1,6 +1,7 @@
 (ns substream.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn input-stream
+  "Create a java.io.InputStream from a side-effectful function that returns
+  the next byte in the stream."
+  [f]
+  (substream.FnInputStream. f))

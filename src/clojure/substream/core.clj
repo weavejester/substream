36e8567990869e10a969ab2fc54a1e886be2a3ea
James Reeves <jreeves@weavejester.com>
2014-01-24 16:27:30 +0000

    Added protocol for converting bytes, longs and nils to ints

--- src/clojure/substream/core.clj
+++ src/clojure/substream/core.clj
@@ -1,7 +1,21 @@
-(ns substream.core)
+(ns substream.core
+  (:import substream.FnInputStream))
+
+(defprotocol ToOctet
+  (to-octet [x]))
+
+(extend-protocol ToOctet
+  Integer
+  (to-octet [x] x)
+  Long
+  (to-octet [x] (Integer/valueOf x))
+  Byte
+  (to-octet [x] (Integer/valueOf (bit-and x 0xff)))
+  nil
+  (to-octet [_] (Integer/valueOf -1)))
 
 (defn input-stream
-  "Create a java.io.InputStream from a side-effectful function that returns
+  "Create a java.io.InputStream from a zero-argument function that returns
   the next byte in the stream."
-  [f]
-  (substream.FnInputStream. f))
+  [func]
+  (FnInputStream. (comp to-octet func)))

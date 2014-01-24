e8e5a2ae2ef83537fe7fae4fbbfed17c89e1c4c2
James Reeves <jreeves@weavejester.com>
2014-01-24 17:13:38 +0000

    Updated description and URL of project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject substream "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for subclassing InputStreams"
+  :url "https://github.com/weavejester/substream"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
